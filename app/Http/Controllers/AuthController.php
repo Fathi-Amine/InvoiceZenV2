bacd5f528728315fd1635a30fe5edfc7d0c5f63d
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-04-13 15:20:57 +0000

    Implementing login and logout with creating a middleware and setting up auth routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'message' => 'Email or password is incorrect'
+            ], 422);
+        }
+
+        /** @var \App\Models\User $user */
+
+        $user = Auth::user();
+        if (!$user->is_admin) {
+            Auth::logout();
+            return response([
+                'message' => 'Rights to authenticate as Admin is refused'
+            ], 403);
+        }
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user;
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
