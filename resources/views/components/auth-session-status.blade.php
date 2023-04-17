f81981127bb8fccc7070cb3304c5b0249ddd5228
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-04-17 23:55:41 +0000

    customizing email templates

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-white bg-green-600 py-3 px-4 rounded']) }}>
         {{ $status }}
     </div>
 @endif
